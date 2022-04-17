d05ad63f9b843f9d229325cdb60daa8b250da2b6
14km <k.connor614@gmail.com>
2022-04-17 22:22:43 +0900

    feat: 숫자 입력시 초기화 처리 추가

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -1,6 +1,6 @@
 package baseball.domain;
 
-import baseball.common.util.NumberUtil;
+import baseball.common.utils.NumberUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,15 +12,17 @@ public class Player {
     public void inputNumber(String inputString) {
         isValid(inputString);
 
+        clear();
         for (String input : inputString.split("")) {
             int result = Integer.parseInt(input);
             inputNumbers.add(result);
         }
     }
 
+    // TODO : 중복된 숫자를 확인하는 검증이 필요함.
     private void isValid(String inputString) {
         if (!NumberUtil.isInteger(inputString)) {
-            throw new IllegalArgumentException("숫자를 입력해주시기 바랍니다. (입력값:" + inputString + ")");
+            throw new IllegalArgumentException("1부터 9까지의 숫자를 입력해주시기 바랍니다. (입력값:" + inputString + ")");
         }
 
         if (inputString.length() > DRAW_COUNT) {
@@ -30,5 +32,9 @@ public class Player {
 
     public List<Integer> getInputNumbers() {
         return inputNumbers;
+    }
+
+    private void clear() {
+        inputNumbers.clear();
     }
 }

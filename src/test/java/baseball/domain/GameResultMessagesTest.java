607040f4c9bd5e64509b080f7c17076aa494823d
14km <k.connor614@gmail.com>
2022-04-18 02:08:22 +0900

    test: GameResultMessagesTest 추가

---
+++ src/test/java/baseball/domain/GameResultMessagesTest.java
@@ -0,0 +1,27 @@
+package baseball.domain;
+
+import baseball.common.enums.GameStatus;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameResultMessagesTest {
+
+    @Test
+    void 결과_메세지_변환_테스트() {
+        // given
+        GameResult gameResult = new GameResult();
+        gameResult.addResult(GameStatus.STRIKE);
+        gameResult.addResult(GameStatus.STRIKE);
+        gameResult.addResult(GameStatus.BALL);
+
+        // when
+        String message = GameResultMessages.convert(gameResult);
+
+        // then
+        Assertions.assertThat(message).contains(
+                String.format("1%s 2%s", GameStatus.BALL.getName(), GameStatus.STRIKE.getName())
+        );
+    }
+}

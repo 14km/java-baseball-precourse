5f195b8853d6482c6ec79fe6f72be31cc6592ffe
14km <k.connor614@gmail.com>
2022-04-17 22:25:45 +0900

    feat: 게임 진행에 필요한 시스템 추가

---
+++ src/main/java/baseball/domain/GameSystem.java
@@ -0,0 +1,44 @@
+package baseball.domain;
+
+import baseball.common.enums.GameStatus;
+
+public class GameSystem {
+    private final Player player;
+    private final Computer computer;
+
+    public GameSystem(Player player, Computer computer) {
+        this.player = player;
+        this.computer = computer;
+    }
+
+    public GameResult play() {
+        isValid();
+
+        GameResult gameResult = new GameResult();
+        for (int i = 0; i < player.getInputNumbers().size(); i++) {
+            int playerNumber = player.getInputNumbers().get(i);
+            int computerNumber = computer.getRandomsNumbers().get(i);
+
+            GameStatus gameStatus = checkStatus(playerNumber, computerNumber);
+            gameResult.addResult(gameStatus);
+        }
+
+        return gameResult;
+    }
+
+    private GameStatus checkStatus(int playerNumber, int computerNumber) {
+        if (computerNumber == playerNumber) return GameStatus.STRIKE;
+        if (computer.getRandomsNumbers().contains(playerNumber)) return GameStatus.BALL;
+        return GameStatus.NOTHING;
+    }
+
+    private void isValid() {
+        if (computer.getRandomsNumbers().size() == 0 || player.getInputNumbers().size() == 0) {
+            throw new IllegalArgumentException("컴퓨터 숫자 또는 플레이어에 숫자가 설정되지 않았습니다.");
+        }
+
+        if (computer.getRandomsNumbers().size() != player.getInputNumbers().size()) {
+            throw new IllegalArgumentException("게임 실행시 필요한 숫자가 일치하지 않습니다.");
+        }
+    }
+}

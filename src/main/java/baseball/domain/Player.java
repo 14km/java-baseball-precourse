78168580422fe7fb978a4f471c949108d159bf52
14km <k.connor614@gmail.com>
2022-04-18 00:33:15 +0900

    refactor: Player Indent 수정

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -15,10 +15,7 @@ public class Player {
         for (String input : inputString.split("")) {
             int result = Integer.parseInt(input);
 
-            if (this.inputNumbers.contains(result)) {
-                throw new IllegalArgumentException(String.format("중복된 숫자는 입력할 수 없습니다. [입력 값: %s, 중복된 숫자: %s]", inputString, input));
-            }
-
+            isDuplicateNumber(result);
             inputNumbers.add(result);
         }
     }
@@ -30,6 +27,12 @@ public class Player {
 
         if (inputString.length() > GameOption.DRAW_COUNT) {
             throw new IllegalArgumentException(String.format("숫자는 %s 자리까지 입력이 가능합니다. (111 ~ 999)", GameOption.DRAW_COUNT));
+        }
+    }
+
+    private void isDuplicateNumber(int result) {
+        if (this.inputNumbers.contains(result)) {
+            throw new IllegalArgumentException(String.format("중복된 숫자는 입력할 수 없습니다. [중복된 숫자: %s]", result));
         }
     }
 

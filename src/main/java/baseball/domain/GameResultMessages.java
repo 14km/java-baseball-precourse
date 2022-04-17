43643e3389ddcc94c2ee489afbc451da735ce174
14km <k.connor614@gmail.com>
2022-04-17 22:24:51 +0900

    feat: GameResultMessages 추가

---
+++ src/main/java/baseball/domain/GameResultMessages.java
@@ -0,0 +1,27 @@
+package baseball.domain;
+
+import baseball.common.enums.GameStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameResultMessages {
+
+    public static String convert(GameResult gameResult) {
+        List<String> resultMessagesList = new ArrayList<>();
+
+        if (gameResult.getBallCounter() > 0) {
+            resultMessagesList.add(String.format("%d%s", gameResult.getBallCounter(), GameStatus.BALL.getName()));
+        }
+
+        if (gameResult.getStrikeCounter() > 0) {
+            resultMessagesList.add(String.format("%d%s", gameResult.getStrikeCounter(), GameStatus.STRIKE.getName()));
+        }
+
+        if (gameResult.getStrikeCounter() == 0 && gameResult.getBallCounter() == 0) {
+            resultMessagesList.add(GameStatus.NOTHING.getName());
+        }
+
+        return String.join("", resultMessagesList).trim();
+    }
+}
